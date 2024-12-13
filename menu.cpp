b798ef111d94ccd642b0fc75c09717d32730aa3a
TiaraNurAzizah-C050423022 <c050423022@mahasiswa.poliban.ac.id>
2024-12-13 12:58:32 +0800

    i: code case 3

--- menu.cpp
+++ menu.cpp
@@ -40,6 +40,17 @@ void tampilkanData(int data[], int n) {
     }
 }
 
+void sortingAscending(int data[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (data[j] > data[j + 1]) {
+                tukar(&data[j], &data[j + 1]);
+            }
+        }
+    }
+    cout << "Data berhasil diurutkan secara ascending!";
+}
+
 int main() {
     int data[100];
     char pl;
@@ -72,10 +83,16 @@ int main() {
                 getch();
                 break;
             }  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
+   case '3': {
+                system("cls");
+                if (n > 0) {
+                    sortingAscending(data, n);
+                } else {
+                    cout << "Data kosong. Masukkan data terlebih dahulu!";
+                }
+                getch();
+                break;
+            } 
    case '4':
     mPertama("ke- empat");
     /* code */
